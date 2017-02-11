5df647b99e983b130383a587ddb6563f485671d3
megasyl <megasyl20@gmail.com>
2017-02-11 19:10:26 +0100

    Adding Mail function

---
+++ Utils/Mail.php
@@ -0,0 +1,66 @@
+<?php
+
+class Mail
+{
+    private $_destinataire;
+    private $_expediteur;
+    private $_objet;
+    private $_headers;
+    private $_message;
+
+    public function __construct($_destinataire, $_expediteur, $_objet, $_message)
+    {
+        $this->_destinataire = $_destinataire;
+        $this->_expediteur = $_expediteur;
+        $this->_objet = $_objet;
+        $this->_headers = 'MIME-Version: 1.0' . "\n";
+        $this->_headers .= 'Content-type: text/html; charset=ISO-8859-1' . "\n";
+        $this->_headers .= 'From: "Marmiton"<' . $this->_expediteur . '>' . "\n"; // Expediteur
+        $this->_headers .= 'Delivered-to: ' . $this->_destinataire . "\n"; // Destinataire
+        $this->_message = "<div style='width: 100%; text-align: center; font-weight: bold'>Votre recette a été acceptée !</div><div>$this->_message</div>";
+    }
+
+    public function getDestinataire()
+    {
+        return $this->_destinataire;
+    }
+
+    public function setDestinataire($destinataire)
+    {
+        $this->_destinataire = $destinataire;
+        return $this;
+    }
+
+    public function getObjet()
+    {
+        return $this->_objet;
+    }
+
+    public function setObjet($objet)
+    {
+        $this->_objet = $objet;
+        return $this;
+    }
+
+    public function getMessage()
+    {
+        return $this->_message;
+    }
+
+    public function setMessage($message)
+    {
+        $this->_message = $message;
+        return $this;
+    }
+
+    public function send()
+    {
+        if (mail($this->_destinataire, $this->_objet, $this->_message, $this->_headers))
+            echo 'Votre message a bien été envoyé ';
+        else
+            print_r( "Votre message n'a pas pu être envoyé : " );
+        print_r(error_get_last());
+
+    }
+
+}
--- controllers/recipeController.php
+++ controllers/recipeController.php
@@ -1,16 +1,11 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: root
- * Date: 03/02/17
- * Time: 01:26
- */
 class recipeController
 {
     public function create() {
         require_once ('models/recipe.php');
         require_once ('models/ingredient.php');
+        require_once ('Utils/Mail.php');
 
         if (isset($_POST["ingredients"]) && isset($_POST["author"]) && isset($_POST["recipe"]))  {
 
@@ -22,10 +17,14 @@ class recipeController
             }
 
             var_dump($hey->create());
+            $mail = new Mail($_POST["author"]["email"],
+                             "megasyl20@gmail.com",
+                             "Recette acceptée !!",
+                             "Félicitations, votre recette est desormais accessible à tous sur notre site Maremiton ! <br>A bientôt !");
+            $mail->send();
             require_once('views/pages/home.php');
         }
         else
         require_once ('views/pages/error.php');
-        var_dump($_POST);
     }
 }
