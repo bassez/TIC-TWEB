7d2e59b59d2dd32ae1d9eb07ca5fd274e9fb07bf
bassez <bassez_f@etna-alternance.net>
2017-02-22 21:33:35 +0100

    Details

--- views/pages/details.php
+++ views/pages/details.php
@@ -1,7 +1,89 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: sylvainlasjunies
- * Date: 22/02/17
- * Time: 15:49
- */
+<div style="border-width: 0 0 1px; padding: 10px;" class="navbar-default container">
+        <div class="col-lg-12" style="margin-top: 50px;border-bottom: solid 1px;border-color: #F47321;">
+
+
+            <div class="col-lg-12">
+                <h1 style="color:#F47321;margin-top: 20px;"><?php echo $datas->getName(); ?></h1>
+            </div>
+
+
+            <div class="col-lg-12">
+                <span class="label label-info" >tags</span>
+                <span class="label label-warning" >tags</span>
+                <span class="label label-danger" >tags</span>
+            </div>
+
+
+            <div class="col-lg-12" style="display: flex;">
+                <h4 style="line-height:2;margin-left: 5px;">STARS</h4><h4 style="line-height:2;margin-left: 5px;">STARS</h4><h4 style="line-height:2;margin-left: 5px;">STARS</h4>
+            </div>
+        </div>
+
+        <div class="col-lg-12" style="border-bottom: solid 1px;border-color: #F47321;">
+
+            <div class="col-lg-4" style="margin-top:15px;height: 200px;background-image: url('/views/img/dog.png')">
+
+            </div>
+
+            <div class="col-lg-8">
+                <div class="col-lg-6" style="margin-top:15px;border-right: solid 1px;border-color: #F47321;">
+                    <div class="col-lg-12" style="display: flex">
+                        <i style="line-height: 2.5;margin-right: 5px;" class="fa fa-cart-plus" aria-hidden="true"></i><h5>difficulty</h5>
+                    </div>
+                    <div class="col-lg-12" style="display: flex">
+                        <i style="line-height: 2.5;margin-right: 5px;" class="fa fa-clock-o" aria-hidden="true"></i><h5>time</h5>
+                    </div>
+                    <div class="col-lg-12">
+                        <h4>Ingredients</h4>
+                        <ul>
+                            <li>
+                                <span>2 grammes de chocolat</span>
+                            </li>
+                            <li>
+                                <span>3 kilogrammes d'heroine</span>
+                            </li>
+                            <li>
+                                <span>5 cuilleres a cafe de soja</span>
+                            </li>
+                            <li>
+                                <span>5 cuilleres a cafe de soja</span>
+                            </li>
+                            <li>
+                                <span>5 cuilleres a cafe de soja</span>
+                            </li>
+                            <li>
+                                <span>5 cuilleres a cafe de soja</span>
+                            </li>
+                            <li>
+                                <span>5 cuilleres a cafe de soja</span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-6" style="margin-top:15px">
+                    <h4>En mode cuisine :</h4>
+                    <ul>
+                        <li>
+                            <span>Origine : Italienne</span>
+                        </li>
+                        <li>
+                            <span>Type : Dessert</span>
+                        </li>
+                        <li>
+                            <span>Gout : salé</span>
+                        </li>
+                        <li>
+                            <span>Gout : épicé</span>
+                        </li>
+                        <li>
+                            <span>Origine : Mexicaine</span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-12" style="">
+            <div
+        </div>
+</div>
